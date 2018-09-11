99c4ace5e95ee42816eccfac4d9d81a82c163034
Jesse Aguirre <jesse@Jesses-MacBook-Pro.local>
2018-09-11 14:48:49 -0700

    circleci in progress

--- database/database.js
+++ database/database.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose.connect(
-  process.env.MONGODB_URI,
+  process.env.MONGODB_URI || "mongodb://localhost/user-reviews",
   { useNewUrlParser: true }
-  // (err, results) => {
-  //   console.log(err, results);
-  // }
 );
 
 const db = mongoose.connection;
