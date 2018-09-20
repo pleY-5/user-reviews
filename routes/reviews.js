9f1fa3771e0003c862cc27297788588f222326cb
Jesse Aguirre <jesse@Jesses-MacBook-Pro.local>
2018-09-20 16:11:55 -0700

    fixed api reviews fixed

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers/reviews.js");
 
-router.get("/:nameOrId", ctrl.fetch);
+router.get("/reviews/:nameOrId", ctrl.fetch);
 // router.post("/", ctrl.update);
 
 module.exports = router;
