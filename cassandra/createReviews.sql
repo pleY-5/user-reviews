01c1e6745c9a7a1d3db6ccce17a81e2fa0d860a2
Charlie Phan <charliegphan@gmail.com>
2018-10-04 08:47:59 -0700

    complete seedings scripts for cassandra

--- cassandra/createReviews.sql
+++ cassandra/createReviews.sql
@@ -1,4 +1,4 @@
-CREATE TABLE reviews(
+CREATE TABLE reviews_by_id(
   review_id int,
   restaurant_id int,
   restaurant_name text,
@@ -12,5 +12,22 @@ CREATE TABLE reviews(
   cool_count int,
   reviews_count int,
   review text,
-  PRIMARY KEY (review_id)
+  PRIMARY KEY (restaurant_id, review_id)
+);
+
+CREATE TABLE reviews_by_name(
+  restaurant_name text,
+  review_id int,
+  restaurant_id int,
+  user_id int,
+  date text,
+  count_star_ratings int,
+  count_checkin int,
+  ratings int,
+  useful_count int,
+  funny_count int,
+  cool_count int,
+  reviews_count int,
+  review text,
+  PRIMARY KEY (restaurant_name, review_id)
 );
