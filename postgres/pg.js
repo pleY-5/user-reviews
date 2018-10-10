c6db6d83d3d110e905e6c1db971dac5c97edf081
Charlie Phan <charliegphan@gmail.com>
2018-10-10 12:30:43 -0700

    change database to ec2 postgres

--- postgres/pg.js
+++ postgres/pg.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const pg = require('pg');
 
-const client = new pg.Pool({
-  host: 'localhost',
+const client = new pg.Client({
+  host: 'ec2-34-219-27-236.us-west-2.compute.amazonaws.com',
   port: 5432,
   user: process.env.PG_USER,
   password: process.env.PG_PASS,
